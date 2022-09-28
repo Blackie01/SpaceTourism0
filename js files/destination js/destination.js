2396beca8dd76c8e27b0530cc034b81369f3bc66
Blackie01 <damola24795@gmail.com>
2022-09-28 23:57:29 +0100

    more edits

--- js files/destination js/destination.js
+++ js files/destination js/destination.js
@@ -1,6 +1,33 @@
-
-function toMars(){
-    let newJumboNumber = "02"
-    document.getElementById('jumboNumber').innerHTML = newJumboNumber
-    localStorage.setItem('textSet', true);
+function onClickOfMoon(event){
+  event.preventDefault()
+  document.getElementById('moon-section').style.display = 'block'
+  document.getElementById('moonName').style.textDecoration = 'underline'
+  document.getElementById('mars-section').style.display = 'none'
+  document.getElementById('europa-section').style.display = 'none'
+  document.getElementById('titan-section').style.display = 'none'
 }
+
+function onClickOfMars(event){
+  event.preventDefault()
+  document.getElementById('moon-section').style.display = 'none'
+  document.getElementById('mars-section').style.display = 'block'
+  document.getElementById('europa-section').style.display = 'none'
+  document.getElementById('titan-section').style.display = 'none'
+}
+
+function onClickOfEuropa(event){
+  event.preventDefault()
+  document.getElementById('moon-section').style.display = 'none'
+  document.getElementById('mars-section').style.display = 'none'
+  document.getElementById('europa-section').style.display = 'block'
+  document.getElementById('titan-section').style.display = 'none'
+}
+
+function onClickOfTitan(event){
+  event.preventDefault()
+  document.getElementById('moon-section').style.display = 'none'
+  document.getElementById('mars-section').style.display = 'none'
+  document.getElementById('europa-section').style.display = 'none'
+  document.getElementById('titan-section').style.display = 'block'
+}
+
