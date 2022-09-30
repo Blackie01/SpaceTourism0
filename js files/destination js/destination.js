8c8476358ef3b8119a5a2f8d86d90d0a4b1b8098
Blackie01 <damola24795@gmail.com>
2022-09-30 23:53:02 +0100

    more js addition

--- js files/destination js/destination.js
+++ js files/destination js/destination.js
@@ -1,48 +1,64 @@
 function onClickOfMoon(event){
   event.preventDefault()
-  document.getElementsByClassName('moon-section').style.display = 'block'
+  document.getElementById('moon-section').style.display = 'block'
   document.getElementById('moonName').style.borderBottom = '2px solid yellow'
-  document.getElementsByClassName('mars-section').style.display = 'none'
+  document.getElementById('hero-image-moon').style.display = 'block'
+  document.getElementById('mars-section').style.display = 'none'
   document.getElementById('marsName').style.borderBottom = 'none'
-  document.getElementsByClassName('europa-section').style.display = 'none'
+  document.getElementById('hero-image-mars').style.display = 'none'
+  document.getElementById('europa-section').style.display = 'none'
   document.getElementById('europaName').style.borderBottom = 'none'
-  document.getElementsByClassName('titan-section').style.display = 'none'
+  document.getElementById('hero-image-europa').style.display = 'none'
+  document.getElementById('titan-section').style.display = 'none'
   document.getElementById('titanName').style.borderBottom = 'none'
+  document.getElementById('hero-image-titan').style.display = 'none'
 }
 
 function onClickOfMars(event){
   event.preventDefault()
-  document.getElementsByClassName('moon-section').style.display = 'none'
+  document.getElementById('moon-section').style.display = 'none'
   document.getElementById('moonName').style.borderBottom = 'none'
-  document.getElementsByClassName('mars-section').style.display = 'block'
+  document.getElementById('hero-image-moon').style.display = 'none'
+  document.getElementById('mars-section').style.display = 'block'
   document.getElementById('marsName').style.borderBottom = '2px solid yellow'
-  document.getElementsByClassName('europa-section').style.display = 'none'
+  document.getElementById('hero-image-mars').style.display = 'block'
+  document.getElementById('europa-section').style.display = 'none'
   document.getElementById('europaName').style.borderBottom = 'none'
-  document.getElementsByClassName('titan-section').style.display = 'none'
+  document.getElementById('hero-image-europa').style.display = 'none'
+  document.getElementById('titan-section').style.display = 'none'
   document.getElementById('titanName').style.borderBottom = 'none'
+  document.getElementById('hero-image-titan').style.display = 'none'
 }
 
 function onClickOfEuropa(event){
   event.preventDefault()
-  document.getElementsByClassName('moon-section').style.display = 'none'
+  document.getElementById('moon-section').style.display = 'none'
   document.getElementById('moonName').style.borderBottom = 'none'
-  document.getElementsByClassName('mars-section').style.display = 'none'
+  document.getElementById('hero-image-moon').style.display = 'none'
+  document.getElementById('mars-section').style.display = 'none'
   document.getElementById('marsName').style.borderBottom = 'none'
-  document.getElementsByClassName('europa-section').style.display = 'block'
+  document.getElementById('hero-image-mars').style.display = 'none'
+  document.getElementById('europa-section').style.display = 'block'
   document.getElementById('europaName').style.borderBottom = '2px solid yellow'
-  document.getElementsByClassName('titan-section').style.display = 'none'
+  document.getElementById('hero-image-europa').style.display = 'block'
+  document.getElementById('titan-section').style.display = 'none'
   document.getElementById('titanName').style.borderBottom = 'none'
+  document.getElementById('hero-image-titan').style.display = 'none'
 }
 
 function onClickOfTitan(event){
   event.preventDefault()
-  document.getElementsByClassName('moon-section').style.display = 'none'
+  document.getElementById('moon-section').style.display = 'none'
   document.getElementById('moonName').style.borderBottom = 'none'
-  document.getElementsByClassName('mars-section').style.display = 'none'
+  document.getElementById('hero-image-moon').style.display = 'none'
+  document.getElementById('mars-section').style.display = 'none'
   document.getElementById('marsName').style.borderBottom = 'none'
-  document.getElementsByClassName('europa-section').style.display = 'none'
+  document.getElementById('hero-image-mars').style.display = 'none'
+  document.getElementById('europa-section').style.display = 'none'
   document.getElementById('europaName').style.borderBottom = 'none'
-  document.getElementsByClassName('titan-section').style.display = 'block'
+  document.getElementById('hero-image-europa').style.display = 'none'
+  document.getElementById('titan-section').style.display = 'block'
   document.getElementById('titanName').style.borderBottom = '2px solid yellow'
+  document.getElementById('hero-image-titan').style.display = 'block'
 }
 
